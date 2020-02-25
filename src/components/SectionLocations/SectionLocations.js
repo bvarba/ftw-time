5b000b2b35f22b57760db962ab2f6de252c3bd4a
Boris Varbanov <varbanov.b@gmail.com>
2020-02-24 23:59:11 -0500

    Customizations landing page. Font color aligning and locations

--- src/components/SectionLocations/SectionLocations.js
+++ src/components/SectionLocations/SectionLocations.js
@@ -51,20 +51,11 @@ const SectionLocations = props => {
       </div>
       <div className={css.locations}>
         {locationLink(
-          'New York',
+          'Montreal',
           nyImage,
-          '?address=New%20York%20City%2C%20New%20York%2C%20USA&bounds=40.917576401307%2C-73.7008392055224%2C40.477399%2C-74.2590879797556'
+          '?address=Montreal%2C%20Quebec%2C%20Canada&bounds=-73.972902%2C45.391752%2C-73.411861%2C45.70479'
         )}
-        {locationLink(
-          'Los Angeles',
-          laImage,
-          '?address=Los%20Angeles%2C%20California%2C%20USA&bounds=34.161440999758%2C-118.121305008073%2C33.9018913203336%2C-118.521456965901'
-        )}
-        {locationLink(
-          'San Francisco',
-          sfImage,
-          '?address=San%20Francisco%2C%20California%2C%20USA&bounds=37.8324430069081%2C-122.354995082683%2C37.6044780500533%2C-122.517910874663'
-        )}
+      
       </div>
     </div>
   );
